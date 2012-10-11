9da65495d13c0aa33b63d562bd22570e84cfe938
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2012-10-11 12:04:52 +0200

    Update ManagePagesCSSResizer.js

--- ManagePagesCSSResizer.js
+++ ManagePagesCSSResizer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Manage Pages CSS Re-sizer
 // @namespace  http://www.mysmallcorneroftheweb.co.uk/
-// @version    0.3
+// @version    0.2
 // @description  Script to automatically re-size the Liferay site-wide CSS box to something more useful
 // @match      http://*/*
 // @copyright  2012+, You
@@ -11,3 +11,6 @@ var regularCSS = document.getElementById('_156_regularCss');
     
 regularCSS.style.width='650px';
 regularCSS.style.height='700px';
+
+regularCSS = document.getElementById('_88_name_en_US');
+regularCSS.style.max-width='350px';
